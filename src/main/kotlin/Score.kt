6a5f2bfe7939b79880941a932390cf827a06333f
fernanda pinho <fbp@ecomp.poli.br>
2022-01-04 20:00:39 -0300

    add score table

--- src/main/kotlin/Score.kt
+++ src/main/kotlin/Score.kt
@@ -4,10 +4,32 @@ fun currentScore(player: Player){
   val score = document.getElementById("score")
   if(score != null){
     val pontos = player.score
-    score.innerHTML = """"
-      |<h1>Total de pontos: $pontos</h1>
-      |
+    score.innerHTML = """
+      |<p>Total de pontos: $pontos</p>
       """.trimMargin()
-
+  }
+  
+  val scoreTable = document.getElementById("score-table")
+  if(scoreTable != null){
+    scoreTable.innerHTML = """
+      <table>
+        <tr>
+          <th>Colocação</th>
+          <th>Pontuação</th>
+        </tr>
+        <tr>
+          <td>1°</td>
+          <td>321</td>
+        </tr>
+        <tr>
+          <td>2°</td>
+          <td>231</td>
+        </tr>
+        <tr>
+          <td>3°</td>
+          <td>123</td>
+        </tr>
+      </table>
+    """.trimIndent()
   }
 }
